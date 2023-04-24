5222679e8840506c829b5d1d6d7bfb6ef6e29fde
AndrewLomonosov <lomonosov.kfu@gmail.com>
2023-04-24 22:27:10 +0200

    Added new filters (lambda) and made Class

--- KeyBoards.py
+++ KeyBoards.py
@@ -3,8 +3,8 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 #Общая клавиатура в чате
 kb_main = ReplyKeyboardMarkup(one_time_keyboard=True, row_width=3, resize_keyboard=True)
-kb1 = KeyboardButton('Клубника')
-kb2 = KeyboardButton('Рассада')
+kb1 = KeyboardButton('/Make_order')
+kb2 = KeyboardButton('/Discounts')
 kb_main.add(kb1,kb2)
 
 #Клавиатура под сообщением с выбором товара (клубника или рассада)
@@ -23,16 +23,28 @@ ikb_S2 = InlineKeyboardButton(text='6 kg',
                               callback_data='value6')
 ikb_S3 = InlineKeyboardButton(text='9 kg',
                               callback_data='value9')
-ikb_S4 = InlineKeyboardButton(text='More',
-                              callback_data='more')
+ikb_S4 = InlineKeyboardButton(text='Заказать больше',
+                              callback_data='value_more')
 ikb_Straw.add(ikb_S1,ikb_S2,ikb_S3).add(ikb_S4)
 
 #Клавиатура для выбора сорта рассады
 ikb_type_seed = InlineKeyboardMarkup(row_width=3)
 ikb_ts1 = InlineKeyboardButton(text='Кабрилло',
-                              callback_data='Seedtype1')
+                               callback_data='Seed_type1')
 ikb_ts2 = InlineKeyboardButton(text='Мурано',
-                              callback_data='Seedtype2')
+                               callback_data='Seed_type2')
 ikb_ts3 = InlineKeyboardButton(text='Мара-де-буа',
-                              callback_data='Seedtype3')
+                               callback_data='Seed_type3')
 ikb_type_seed.add(ikb_ts1,ikb_ts2,ikb_ts3)
+
+#Клавиатура для выбора количества кустов рассады
+ikb_volume_seed = InlineKeyboardMarkup(row_width=3)
+ikb_vs1 = InlineKeyboardButton(text='128 кустов',
+                               callback_data='Seed_volume1')
+ikb_vs2 = InlineKeyboardButton(text='256 кустов',
+                               callback_data='Seed_volume2')
+ikb_vs3 = InlineKeyboardButton(text='512 кустов',
+                               callback_data='Seed_volume3')
+ikb_vs4 = InlineKeyboardButton(text='Свой вариант количества кустов',
+                               callback_data='Seed_more')
+ikb_volume_seed.add(ikb_vs1,ikb_vs2,ikb_vs3).add(ikb_vs4)
